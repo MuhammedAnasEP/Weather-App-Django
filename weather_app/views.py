05a1355e8b79dc7d96ac95cb1c01737ccc459cde
MuhammedAnasEP <anasep1110@gmail.com>
2023-08-02 17:39:21 +0530

    created api_key using openweather map and i modified index view

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def index(request):
+    api_key = open("API_KEY","r").read()
+    print(api_key)
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
+    city = 'Kerala'
+    city_weather = requests.get(url.format(city,api_key)).json()
+    print(city_weather)
     return render(request, 'weather_app/index.html')
