2a9f2e7b085f39c13383575a5fa8ce4d4f1f0e42
MuhammedAnasEP <anasep1110@gmail.com>
2023-08-02 20:29:15 +0530

    finally

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,25 +1,43 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 # Create your views here.
 
 def index(request):
     api_key = open("API_KEY","r").read()
-    print(api_key)
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
-    city = 'malappuram'
-    city_weather = requests.get(url.format(city,api_key)).json()
+    if request.method == 'POST': 
+        name = request.POST['name']
+        try:
+            city_weather = requests.get(url.format(name,api_key)).json()
+            temprature = round((city_weather['main']['temp']-32)*(5/9),2)
+            try:
+                old = City.objects.get(name=name)
+                old.delete()
+            except:                
+                new_name = City.objects.create(name=name)
+                new_name.save()
+            else:
+                new_name = City.objects.create(name=name)
+                new_name.save()
+        except:
+            return render(request, 'weather_app/index.html',{'message':'invalid data'})
 
-    weather = {
+    cities = City.objects.all().order_by("-id")[:4]
+    weather_data = []
+
+    for city in cities:
+        city_weather = requests.get(url.format(city,api_key)).json()
+        weather = {
         'city': city,
         'temprature': round((city_weather['main']['temp']-32)*(5/9),2),
         'description': city_weather['weather'][0]['description'],
         'icon': city_weather['weather'][0]['icon']
-    }
+        }
+        weather_data.append(weather)
 
     context = {
-        'weather': weather
+        'weather_data' : weather_data
     }
-
-    print(city_weather)
     return render(request, 'weather_app/index.html',context)
